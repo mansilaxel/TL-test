d7936904a77bfc9393be04bef4567ce063f3639f
Vanheule Axel <axel.vanheule@gmail.com>
2018-09-16 12:37:05 +0200

    Add basic testing
    Add phpunit and orderTest.php

---
+++ tests/unit/OrderTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 16/09/2018
+ * Time: 12:16
+ */
+
+namespace Test\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Test\Domain\Customer;
+use Test\Domain\Order;
+use Test\Domain\OrderLine;
+use Test\Domain\Product;
+
+final class OrderTest extends TestCase
+{
+
+    //To run tests : \vendor\bin>phpunit --bootstrap ../../vendor/autoload.php ../../tests
+    /**
+     * @test
+     */
+    public function itCanMakeOrderRight(): void
+    {
+        $customer = new Customer('Teamleader', 100000);
+        $product = new Product('Koerstruitje', 69, 50);
+        $orderline1 = new OrderLine($product, 20);
+        $orderline2 = new OrderLine($product, 30);
+        $orderlines = array(
+            'Gent' => $orderline1,
+            'Andere' => $orderline2
+        );
+        $order = new Order($customer, $orderlines);
+
+        $this->assertEquals(
+            $order->getCustomer()->getName(),
+            $customer->getName()
+        );
+
+        $this->assertEquals(
+            $order->getOrderLines()['Gent']->getTotal(),
+            1000
+        );
+
+        $this->assertEquals(
+            $order->getTotal(),
+            2500
+        );
+    }
+}

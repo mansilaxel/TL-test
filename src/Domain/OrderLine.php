98f491f4c7e2b0c45cfa046f1e9a690cd314bef7
Vanheule Axel <axel.vanheule@gmail.com>
2018-09-16 12:01:23 +0200

    Add Orderline model

---
+++ src/Domain/OrderLine.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Axel
+ * Date: 16/09/2018
+ * Time: 11:57
+ */
+
+namespace Test\Domain;
+
+class OrderLine
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    /**
+     * @var int
+     */
+    private $quantity;
+
+    /**
+     * @var float
+     */
+    private $total;
+
+    /**
+     * @param Product $product
+     * @param int $quantity
+     */
+    public function __construct(
+        Product $product,
+        int $quantity
+    )
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+        $this->total = $product->getPrice() * $quantity;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct(Product $product): void
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int $quantity
+     */
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotal(): float
+    {
+        return $this->total;
+    }
+
+    /**
+     * @param float $total
+     */
+    public function setTotal(float $total): void
+    {
+        $this->total = $total;
+    }
+
+}
